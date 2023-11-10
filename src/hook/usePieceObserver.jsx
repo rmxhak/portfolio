52f58ba1ee19cfa9643b6d4a8492cbed2fec9d02
rmxhak <rmxhak@naver.com>
2023-11-10 19:41:07 +0900

    feat: Finish About section Component
    - about section 마크업 완료
    - 요소를 관찰하면서  애니메이션을 동작할 intersection observer hook 생성

---
+++ src/hook/usePieceObserver.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from 'react';
+
+const UsePieceObserver = () => {
+  const targetRef = useRef(null);
+
+  useEffect(() => {
+    const options = { rootMargin: '0px 0px -20% 0px' };
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) return;
+        [...entry.target.querySelectorAll('.unanimate')].forEach((el) =>
+          el.classList.remove('unanimate')
+        );
+      });
+    }, options);
+
+    const childrenNodes = Array.from(targetRef.current?.children);
+
+    childrenNodes.forEach((box) => observer.observe(box));
+  }, []);
+
+  return targetRef;
+};
+
+export default UsePieceObserver;
